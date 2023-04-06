f2e94cbdadcf5de375429d3ec8da456cf506c70c
LDZ-27 <43908812+LDZ-27@users.noreply.github.com>
2023-04-06 19:14:24 +0800

    Add files via upload

--- www/config.php
+++ www/config.php
@@ -1,17 +1,15 @@
 <?php
-// clickhouse 数据库配置在 search.php 
-// sgk 功能目前没开发完善，而且不是重点，可以忽略
      $config=array(
-        'username' => 'admin', //登录帐号
-        'password' => 'admin', // 登录密码
+        'username' => 'admin',
+        'password' => 'admin',
         'fofa_key' =>'',
-        'fofa_email' =>'', // 目前用处不大
-        'xbook_key' =>'', // 微步api，后续会开发不少基于这个的，建议搞一个
-        'github_api' =>'', // github api 免费，涉及一个功能
-        'aiqicha_cookie' => '', // 爱企查 cookie 涉及两个功能
-        'gaode_apikey' => '' //白嫖，最好整一个 涉及两个功能
-         
+        'fofa_email' =>'',
+        'xbook_key' =>'',
+        'github_api' =>'',
+        'aiqicha_cookie' => '',
+        'gaode_apikey' => '',
     );
+    
 function getconfig($type,$config)
 {
     foreach($config as $keyname => $key)
@@ -35,6 +33,7 @@ if(getconfig('username',$config)==$COOKIE['username'])
 setcookie('username','');
 setcookie('password','');
 header("Location: /login.html");
+exit;
 }
 
 
